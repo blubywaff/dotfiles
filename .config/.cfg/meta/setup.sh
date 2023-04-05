bfaf19117b0b5eefbb4749ea9b6453b7bed2c967
Blubywaff <45488563+Blubywaff@users.noreply.github.com>
2023-04-05 10:14:16 -0500

    fix meta/setup

--- .config/.cfg/meta/setup.sh
+++ .config/.cfg/meta/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p $HOME/.config/.cfg/.git-dir/
 /bin/git clone --bare git@github.com:Blubywaff/dotfiles.git
-/bin/git --git-dir=$HOME/.cfg/.git-dir/ --work-tree=$HOME checkout -f
-
+/bin/git --git-dir=$HOME/.config/.cfg/.git-dir/ --work-tree=$HOME checkout -f
+/bin/git --git-dir=$HOME/.config/.cfg/.git-dir/ --work-tree=$HOME config status.showUntrackedFiles no
