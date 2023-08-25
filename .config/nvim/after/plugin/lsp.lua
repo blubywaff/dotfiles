9e70d5e1b94c09eacf7ed5ff642c466a5e034015
blubywaff <45488563+blubywaff@users.noreply.github.com>
2023-08-25 15:05:59 -0500

    Revert "nvim add scheme support with racket" bc remove racket
    This reverts commit d9b7e4cc525f9b1f0e1ee8c43425d64bd77214f2.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -8,10 +8,6 @@ lspconfig.clangd.setup({
     cmd={'-header-insertion=never'}
 })
 
-lspconfig.racket_langserver.setup{
-    cmd={'racket', '-l', 'racket-langserver'}
-}
-
 local cmp = require('cmp')
 local cmp_select = {behaviour = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -38,10 +34,8 @@ lsp.setup_nvim_cmp({
     },
 })
 
---[[
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
-    print("attached")
 	vim.keymap.set("n", "<leader>lgd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>lvs", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -54,26 +48,5 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>laf", function() vim.lsp.buf.format() end, opts)
 	vim.keymap.set("n", "<leader>lvh", function() vim.lsp.buf.signature_help() end, opts)
 end)
---]]
-
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        (function(client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
-            print("attached")
-            vim.keymap.set("n", "<leader>lgd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>lvs", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>ldf", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<leader>ldn", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "<leader>ldp", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>lac", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>lvr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>lan", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("n", "<leader>laf", function() vim.lsp.buf.format() end, opts)
-            vim.keymap.set("n", "<leader>lvh", function() vim.lsp.buf.signature_help() end, opts)
-        end)(args.data.client_id, args.buf)
-    end
-})
 
 lsp.setup()
--- .config/nvim/ftplugin/scheme.lua
+++
@@ -1,5 +0,0 @@
-print("Hello. Scheme")
-vim.lsp.start({
-    name = "Racket Language Server",
-    cmd = {"racket", "-l", "racket-langserver"},
-})
