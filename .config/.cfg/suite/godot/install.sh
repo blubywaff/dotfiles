72557be2133630d3079a74f61c8304849b0e88a5
Blubywaff <45488563+Blubywaff@users.noreply.github.com>
2023-11-12 18:29:33 -0600

    add installation for godot and aseprite

---
+++ .config/.cfg/suite/godot/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install build prerequisites
+sudo apt-get install -y \
+  build-essential \
+  scons \
+  pkg-config \
+  libx11-dev \
+  libxcursor-dev \
+  libxinerama-dev \
+  libgl1-mesa-dev \
+  libglu-dev \
+  libasound2-dev \
+  libpulse-dev \
+  libudev-dev \
+  libxi-dev \
+  libxrandr-dev
+
+git clone git@github.com:godotengine/godot.git repo
+cd repo
+
+# Go to latest stable release
+git_rev="$(git tag -l --sort=refname | grep stable | tail -1)"
+git checkout "$git_rev"
+
+# Create editor binary
+scons platform=linuxbsd lto=full target=editor production=yes
+
+# Create export template with encryption key
+openssl rand -hex 32 > ../godot.gdkey
+export SCRIPT_AES256_ENCRYPTION_KEY="$(cat ../godot.gdkey)"
+scons platform=linuxbsd target=template_release production=yes
+scons platform=linuxbsd target=template_debug
