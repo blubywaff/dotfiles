651ba6092da82db06cbd6a28e3332fe66b238026
Blubywaff <45488563+Blubywaff@users.noreply.github.com>
2023-11-21 01:42:12 -0600

    i3 add scratchpad bindings

--- .config/i3/construct.sh
+++ .config/i3/construct.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# verify dependencies
+sudo apt-get install -y j4-dmenu-desktop
+
 base=$HOME/.config/i3
 cat $(find $base/*.$machine_type -type f | sort) $(find $base/*.common -type f | sort) > $base/config 2> /dev/null
---
+++ .config/i3/unhide.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+i3-input -F '[con_mark="%s"] scratchpad show; [con_mark="%s"] floating toggle' -P 'Show: '
